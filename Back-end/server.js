ea1abc74d5057320412877e1c9e3bc4e59673710
Dobromir Yordanov <shadowword9@gmail.com>
2024-10-19 14:27:00 +0300

    MERN Crash Course - Front-end part
    1.Created Pages:
    -HomePage
    -CreatePage
    -UpdatePage
    
    2.Components:
    -NavBar
    -ProductCard
    
    3.Store API zustand used

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productsRouter from "./routes/product.route.js";
 
@@ -8,9 +9,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows to accept json data in req.body
 
 app.use("/api/products", productsRouter)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   connectDB();
